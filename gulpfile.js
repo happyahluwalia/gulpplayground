f07e358229e52f90d493108e4f4f7697fc0786ed
happyahluwalia <happyahluwalia@gmail.com>
2017-05-15 09:31:52 -0400

    Used Autoprefixer for css file

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp');
 // include gulp-uglify
 var uglify = require('gulp-uglify');
 
+// include prefixer package
+var autoprefixer = require('gulp-autoprefixer');
+
 
 // include browserSync
 
@@ -11,6 +14,17 @@ var uglify = require('gulp-uglify');
 gulp.task('minify', function() {
     gulp.src('js/main.js')
         .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
+//run "gulp minify" to run minification on main.js
+
+// Prefix task
+gulp.task('processCSS', function() {
+    gulp.src('styles/main.css')
+        .pipe(autoprefixer ({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('build'));
 });
 
